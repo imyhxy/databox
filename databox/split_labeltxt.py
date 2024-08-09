6f9ea1ed53e55bcff8d6179029ee3c52b2105c34
imyhxy <imyhxy@gmail.com>
2024-08-09 17:15:31 +0800

    feat: update split_labeltxt

--- databox/split_labeltxt.py
+++ databox/split_labeltxt.py
@@ -2,29 +2,30 @@
 # File: split_labeltxt.py
 # Date: 12/5/23
 """
-Split label text file created by anno2labeltxt.py into non-overlapping splits.
+Split label text file into stratified shuffle split.
 """
-
 import argparse
 from collections import defaultdict
 from pathlib import Path
 
-from utils.general import calculate_split_ratios
+import yaml
+from sklearn.model_selection import train_test_split
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    exclusive_group = parser.add_mutually_exclusive_group(required=True)
+    exclusive_group.add_argument("--yaml", action="store_true", help="use params.yaml")
+    exclusive_group.add_argument("--seed", type=int, help="random seed")
+
+    parser.add_argument("--train", type=float, default=0.8, help="train split ratio")
+    parser.add_argument("--val", type=float, default=0.2, help="validation split ratio")
+
     parser.add_argument(
         "--label-txt",
         type=str,
         required=True,
         help="Path to label text file, each line: '/path/to/image cid'",
-    )
-    parser.add_argument(
-        "--split-ratios",
-        type=str,
-        default="8:1:1",
-        help="A colon separated list of weights of splits",
     )
 
     return parser.parse_args()
@@ -56,10 +57,25 @@ def split_labeltxt(
 def main():
     args = parse_args()
 
-    split_ratios = calculate_split_ratios(args.split_ratios)
-    splits = split_labeltxt(args.label_txt, split_ratios)
+    if args.yaml:
+        params = yaml.safe_load(open("params.yaml"))["split_labeltxt"]
+        train = params["train"]
+        seed = params["seed"]
+    else:
+        train = args.train
+        seed = args.seed
 
-    preset_names = ["train", "val", "test"]
+    assert 0 < train < 1, f"train ratio must be between 0 and 1, got {train}"
+
+    text = [line.strip() for line in open(args.label_txt).readlines() if line.strip()]
+    y = [int(r.rsplit(maxsplit=1)[1]) for r in text]
+
+    x_train, x_val, y_train, y_val = train_test_split(
+        text, y, train_size=train, random_state=seed, shuffle=True, stratify=y
+    )
+
+    splits = [x_train, x_val]
+    preset_names = ["train", "val"]
     for idx, split in enumerate(splits):
         if idx < 3:
             name = preset_names[idx]
@@ -67,7 +83,7 @@ def main():
             name = f"split{idx}"
 
         with Path(args.label_txt).with_stem(name).open("w") as f:
-            f.writelines(split)
+            f.write("\n".join(split))
 
 
 if __name__ == "__main__":
